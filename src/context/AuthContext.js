ae14ef847d6e6d73dfaf318732de678ed273587a
Royce Chua <royce236@gmail.com>
2020-09-05 16:37:17 +0800

    Add handler for duplicate sign up info

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -3,15 +3,22 @@ import trackerApi from '../api/tracker';
 
 const authReducer = (state, action) => {
     switch (action.type) {
+        case 'add_error':
+            return {...state, errorMessage: action.payload }
         default:
             return state;
     }
 };
 
 const signup = (dispatch) => {
-    return ({ email, password }) => {
-        
-
+    return async ({ email, password }) => {
+        try {
+            const response = await trackerApi.post('/signup', { email, password })
+            console.log(response.data)
+        } catch (err) {
+            // console.log(err.message)
+            dispatch({ type: 'add_error', payload: 'Something went wrong with sign up' })
+        }
     };
 };
 
@@ -33,5 +40,5 @@ const signout = (dispatch) => {
 export const { Provider, Context } = CreateDataContext(
     authReducer,
     { signin, signout, signup},
-    { isSignedIn: false }
+    { isSignedIn: false, errorMessage: '' }
 );
