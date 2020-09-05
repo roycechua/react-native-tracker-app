e767b7a6ef0a5f8747b7ebc48457af6dd40b1ad2
Royce Chua <royce236@gmail.com>
2020-09-05 19:35:25 +0800

    Create extract sign up form to Authform

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,10 +1,14 @@
+import AsyncStorage from '@react-native-community/async-storage';
 import CreateDataContext from "./createDataContext";
 import trackerApi from '../api/tracker';
+import { navigate } from '../navigationRef'
 
 const authReducer = (state, action) => {
     switch (action.type) {
         case 'add_error':
             return {...state, errorMessage: action.payload }
+        case 'signup':
+            return { errorMessage: '', token: action.payload }
         default:
             return state;
     }
@@ -14,7 +18,10 @@ const signup = (dispatch) => {
     return async ({ email, password }) => {
         try {
             const response = await trackerApi.post('/signup', { email, password })
-            console.log(response.data)
+            await AsyncStorage.setItem('token', response.data.token)
+            dispatch({ type: 'signup', payload: response.data.token })
+
+            navigate('TrackList');
         } catch (err) {
             // console.log(err.message)
             dispatch({ type: 'add_error', payload: 'Something went wrong with sign up' })
@@ -36,9 +43,8 @@ const signout = (dispatch) => {
     }
 }
 
-
 export const { Provider, Context } = CreateDataContext(
     authReducer,
     { signin, signout, signup},
-    { isSignedIn: false, errorMessage: '' }
+    { token: null, errorMessage: '' }
 );
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,42 +1,23 @@
-import React, { useState, useContext } from "react";
-import { View, StyleSheet } from "react-native";
-import { Text, Input, Button } from "react-native-elements";
-import Spacer from "../components/Spacer";
+import React, { useContext } from "react";
+import { View, StyleSheet, TouchableOpacity } from "react-native";
+import { Text } from "react-native-elements";
 import { Context as AuthContext } from "../context/AuthContext";
+import AuthForm from '../components/Authform';
 
 const SignupScreen = ({ navigation }) => {
     const { state, signup } = useContext(AuthContext);
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
 
     return (
         <View style={styles.container}>
-            <Spacer>
-                <Text h3>Sign Up for Tracker</Text>
-            </Spacer>
-            <Input
-                label="Email"
-                value={email}
-                onChangeText={setEmail}
-                autoCapitalize="none"
-                autoCorrect={false}
+            <AuthForm
+                headerText="Sign up for Tracker"
+                errorMessage={state.errorMessage}
+                submitButtonText="Sign Up"
+                onSubmit={signup}
             />
-            <Spacer />
-            <Input
-                secureTextEntry
-                label="Password"
-                value={password}
-                onChangeText={setPassword}
-                autoCapitalize="none"
-                autoCorrect={false}
-            />
-            {state.errorMessage ? <Text style={styles.errorMessage}>{state.errorMessage}</Text> : null}
-            <Spacer>
-                <Button
-                    title="Sign Up"
-                    onPress={() => signup({ email, password })}
-                />
-            </Spacer>
+            <TouchableOpacity onPress={() => navigation.navigate('Signin')}>
+                <Text style={styles.link}>Already have an account? Sign in instead</Text>
+            </TouchableOpacity>
         </View>
     );
 };
@@ -53,11 +34,8 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         marginBottom: 250,
     },
-    errorMessage: {
-        fontSize: 16,
-        color: 'red',
-        marginLeft: 15,
-        marginTop: 15,
+    link: {
+        color: 'blue',
     }
 });
 
