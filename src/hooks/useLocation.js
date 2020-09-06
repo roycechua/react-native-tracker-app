acb4917d46c3130afd0d18c4170953720b142fe0
Royce Chua <royce236@gmail.com>
2020-09-06 22:16:46 +0800

    Extract reusable location code to  Custom Hook

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -1,32 +1,35 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 import {
-    Accuracy,
-    requestPermissionAsync,
-    watchPositionAsync,
-} from "expo-location";
+  Accuracy,
+  requestPermissionsAsync,
+  watchPositionAsync,
+} from 'expo-location';
 
 export default (callback) => {
-    const [err, setErr] = useState(null);
+  const [err, setErr] = useState(null);
 
-    const startWatching = async () => {
-      try {
-        const { granted } = await requestPermissionsAsync();
-        if (!granted) {
-          throw new Error('Location permission not granted');
-        }
-        await watchPositionAsync({
+  const startWatching = async () => {
+    try {
+      const { granted } = await requestPermissionsAsync();
+      if (!granted) {
+        throw new Error('Location permission not granted');
+      }
+      await watchPositionAsync(
+        {
           accuracy: Accuracy.BestForNavigation,
           timeInterval: 1000,
           distanceInterval: 10,
-        }, callback)
-      } catch (e) {
-        setErr(e);
-      }
-    };
+        },
+        callback
+      );
+    } catch (e) {
+      setErr(e);
+    }
+  };
 
-    useEffect(() => {
-        startWatching();
-    }, [])
+  useEffect(() => {
+    startWatching();
+  }, []);
 
-    return [err, ]
+  return [err];
 };
