ffcd7936d6d4f42bb9e383463c359c60a0389b4e
Royce Chua <royce236@gmail.com>
2020-09-21 22:30:03 +0800

    Add icons to tab and render track detail

--- src/screens/TrackListScreen.js
+++ src/screens/TrackListScreen.js
@@ -11,7 +11,6 @@ const TrackListScreen = ({ navigation }) => {
     return (
         <>
         <NavigationEvents onWillFocus={fetchTracks}/>
-        <Text style={{ fontSize: 48 }}>TrackListScreen</Text>
         <FlatList
             data={state}
             keyExtractor={(item => item._id)}
@@ -25,7 +24,11 @@ const TrackListScreen = ({ navigation }) => {
         />
         </>
     );
-}
+};
+
+TrackListScreen.navigationOptions = {
+    title: 'Tracks'
+};
 
 const styles = StyleSheet.create({
 
