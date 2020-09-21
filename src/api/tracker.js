2527ca272824d0ed6777c205ac6d056c6d5a21f9
Royce Chua <royce236@gmail.com>
2020-09-21 21:34:24 +0800

    add bearer token to API

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,21 @@
 import axios from 'axios';
+import AsyncStorage from '@react-native-community/async-storage';
 
-export default axios.create({
-    baseURL: 'http://02bdefd15007.ngrok.io'
-})
+const instance = axios.create({
+    baseURL: 'http://ce1dc5c9830b.ngrok.io'
+});
+
+instance.interceptors.request.use(
+    async (config) => {
+        const token = await AsyncStorage.getItem('token');
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+        return config
+    },
+    (err) => {
+        return Promise.reject(err)
+    },
+);
+
+export default instance;
